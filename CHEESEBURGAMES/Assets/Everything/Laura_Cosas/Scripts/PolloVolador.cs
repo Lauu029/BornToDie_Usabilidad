a99ae50e6df970ab68b233df64a7e5103489d23a
Lauu029 <lauragbodego@gmail.com>
2021-04-12 23:49:58 +0200

    Movimiento del bicho volador

---
+++ CHEESEBURGAMES/Assets/Everything/Laura_Cosas/Scripts/PolloVolador.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolloVolador : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+
+    float horizontal, vertical;
+
+    Vector2 movimiento;
+
+    public float velocidad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        movimiento = new Vector2(horizontal, vertical);
+        movimiento.Normalize();
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = movimiento * velocidad;
+    }
+
+
+}
