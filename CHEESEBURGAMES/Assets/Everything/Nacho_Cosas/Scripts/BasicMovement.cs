4525fd074da758801d76a1e12cc7ff472154dbbb
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2021-04-12 23:17:36 +0200

    Cambios en mi escena

---
+++ CHEESEBURGAMES/Assets/Everything/Nacho_Cosas/Scripts/BasicMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    // Valores modificables
+    [Header("VALORES MODIFICABLES")]
+    [SerializeField]
+    float lateralVelocity;
+    [SerializeField]
+    float jumpForce;
+    [SerializeField]
+    float gravityScale;
+
+    // Referencias
+    [Header("REFERENCIAS")]
+    [SerializeField]
+    Transform gfx;
+
+    // Configuracion
+    Rigidbody2D rb;
+    OnGround onG;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        onG = GetComponentInChildren<OnGround>();
+
+        rb.gravityScale = gravityScale;
+    }
+
+    private void Update()
+    {
+        LateralMovement();
+
+        CheckForTrampoline();
+
+        Jump();
+    }
+
+    void LateralMovement()
+    {
+        // Move
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xAxis * lateralVelocity, rb.velocity.y);
+
+        // Actualizar direccion del sprite
+        if (rb.velocity.x > 0) gfx.localScale = new Vector2(1, 1);
+        else if (rb.velocity.x < 0) gfx.localScale = new Vector2(-1, 1);
+    }
+
+    void Jump()
+    {
+        if ((Input.GetKeyDown("w") || Input.GetKeyDown("space")) && onG.touchingGround)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    void CheckForTrampoline() // Comprueba si se ha colisionado con un trampolin, y en tal, caso aplica la fuerza necesaria
+    {
+        Trampoline_Minion trampolineScript = null;
+
+        if (onG.objTouching != null)
+            trampolineScript = onG.objTouching.GetComponent<Trampoline_Minion>();
+
+        if (trampolineScript != null)
+            rb.velocity = new Vector2(rb.velocity.x, trampolineScript.trampolineJumpForce);
+    }
+}
---
+++ CHEESEBURGAMES/Assets/Everything/Nacho_Cosas/Scripts/Minions/Trampoline_Minion.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline_Minion : MonoBehaviour
+{
+    [HideInInspector]
+    public float trampolineJumpForce = 20;
+}
