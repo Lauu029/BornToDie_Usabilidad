be24dcefb2d333d144fbd1fd48dd7de6aca6545d
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2021-04-13 22:29:21 +0200

    wdefgrthy

---
+++ CHEESEBURGAMES/Assets/WInMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WInMenu : MonoBehaviour
+{
+    bool canSkip = false;
+
+    private void Awake()
+    {
+        canSkip = false;
+        Invoke("CanSkip", 1);
+    }
+
+    void CanSkip()
+    {
+        canSkip = true;
+    }
+
+    private void Update()
+    {
+        if (canSkip && Input.anyKeyDown)
+            SceneManager.LoadScene("MainMenu");
+    }
+}
