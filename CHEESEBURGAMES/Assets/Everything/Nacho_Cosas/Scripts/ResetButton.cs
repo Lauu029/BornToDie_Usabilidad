afa170963053a18cea8e1bda7d67f98560956c41
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2021-04-14 16:51:46 +0200

    wdefrg

--- CHEESEBURGAMES/Assets/Everything/Nacho_Cosas/Scripts/ResetButton.cs
+++ CHEESEBURGAMES/Assets/Everything/Nacho_Cosas/Scripts/ResetButton.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
-public class ResetButton : MonoBehaviour
+public class ResetButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    bool onPointer;
+    Animator thisAnimator;
+
+    private void Awake()
     {
-        
+        thisAnimator = GetComponent<Animator>();
+
+        Debug.Log("lskmcfvhbdjc");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (onPointer && Input.GetMouseButtonDown(0))
+        {
+            GameManager.GetInstance().ChangeLevel(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    //POINTER ENTER/EXIT // Quitar los dos metodos siguientes en el caso de que se quiera un menu solo controlado por input de teclado/mando
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        thisAnimator.SetBool("Selected", true);
+        GetComponentInChildren<Text>().color = Color.red;
+
+        onPointer = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        thisAnimator.SetBool("Selected", false);
+        GetComponentInChildren<Text>().color = Color.black;
+
+        onPointer = false;
     }
 }
