aa371cfbf8bcdde50d92212fa310b90109185307
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2021-04-15 09:56:34 +0200

    Revert "sdfgh"
    This reverts commit d8a2d44afa4ef7eb6e3fecdb168baf90032b85ec.

--- CHEESEBURGAMES/Assets/Everything/Nacho_Cosas/Scripts/Minions/Trampoline_Minion.cs
+++ CHEESEBURGAMES/Assets/Everything/Nacho_Cosas/Scripts/Minions/Trampoline_Minion.cs
@@ -16,6 +16,7 @@ public class Trampoline_Minion : MonoBehaviour
                 if (colPos.x > transform.position.x - transform.localScale.x/2 && colPos.x < transform.position.x + transform.localScale.x / 2) // Comprobar si no esta lejos
                     collision.GetComponentInParent<BasicMovement>().GoUp(trampolineJumpForce); // Aplicar fuerza de salto
         }
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
