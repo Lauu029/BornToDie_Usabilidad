bd67ae030ee459593ca8a3e46d635ad59bc64145
Laura Gomez <lgomez25@ucm.es>
2024-03-01 12:05:10 +0100

    Se ven los clicks del raton

--- CHEESEBURGAMES/Assets/MouseCursor.cs
+++ CHEESEBURGAMES/Assets/MouseCursor.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseCursor : MonoBehaviour
 {
-
+    [SerializeField]
+    Image cursor;
+    double timer;
+    Color cursorColor;
     private void Awake()
     {
+        cursorColor = cursor.color;
         Cursor.visible = false;
     }
 
@@ -16,5 +21,17 @@ public class MouseCursor : MonoBehaviour
         //Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 cursorPos = Input.mousePosition;
         transform.position = cursorPos;
+        if (Input.GetMouseButtonDown(0))
+        {
+            cursor.color = Color.black;
+            timer = 0;
+        }
+        if (cursor.color == Color.black)
+            timer += Time.deltaTime;
+        if(timer>=0.15 && cursor.color == Color.black)
+        {
+            cursor.color = cursorColor;
+            
+        }
     }
 }
