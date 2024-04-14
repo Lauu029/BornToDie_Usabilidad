48c9adfeadde18c75808a9886c6ac13128aefad7
Miguel González Pérez <miggon23@ucm.es>
2024-04-14 21:38:51 +0200

    Carpeta Plugins/TeleetrySystem y archivo Tracker.cs

---
+++ CHEESEBURGAMES/Assets/Plugins/TelemetrySystem/Tracker.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Tracker : MonoBehaviour
+{
+    private static Tracker instance = null;
+    private static bool initialized = false;
+    public static Tracker Instance
+    {
+        get { 
+            Assert.IsTrue(initialized);
+            return instance; 
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static bool Init() //Eventos de inicio de sesión, plataforma, SO...
+    {
+        Assert.IsTrue(!initialized);
+        //Al ser monobehaviour no hay que crear nueva instancia
+        //Creación de una posible hebra que volcará los datos
+        initialized= true;
+        return true;
+    }
+
+    public static bool End()
+    {
+        Assert.IsTrue(initialized);
+        //Volcado de los datos restantes
+        //Cierre de la posible hebra 
+        initialized= false;
+        return true;
+    }
+}
