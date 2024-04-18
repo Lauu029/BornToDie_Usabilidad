276b8eeb875e7573a618d35894595bdb3a7b094e
Monchi <lgomez25@ucm.es>
2024-04-18 20:39:37 +0200

    los eventos bien hechos

--- CHEESEBURGAMES/Assets/Plugins/TelemetrySystem/TrackerEvents/ClickGo.cs
+++ CHEESEBURGAMES/Assets/Plugins/TelemetrySystem/TrackerEvents/ClickGo.cs
@@ -11,17 +11,6 @@ public class ClickGo : TrackerEvent
     }
     public override string ToJson()      
     {
-        DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(ProgressionEvent));
-        string json = "";
-        // Crear un MemoryStream para escribir el JSON serializado
-        using (MemoryStream stream = new MemoryStream())
-        {
-            // Serializar el objeto y escribirlo en el MemoryStream
-            serializer.WriteObject(stream, this);
-
-            // Convertir el MemoryStream a una cadena JSON
-             json = System.Text.Encoding.UTF8.GetString(stream.ToArray());
-        }
-        return json;
+       return base.ToJson();
     }
 }
