62072a8e0aabab363790e719180171416518e3d5
Miguel González Pérez <miggon23@ucm.es>
2024-04-18 20:22:40 +0200

    Session start event

---
+++ CHEESEBURGAMES/Assets/Plugins/TelemetrySystem/TrackerEvents/SessionStartEvent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class SessionStartEvent : TrackerEvent
+{
+    public SessionStartEvent() { }
+
+    public override string ToJson()
+    {
+        return base.ToJson();
+    }
+}
