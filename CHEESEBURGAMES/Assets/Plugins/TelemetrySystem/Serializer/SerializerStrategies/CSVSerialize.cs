dc48d53f943fbe410043a0072ea2737a2e6ce6a5
miriam-m-s <81752837+miriam-m-s@users.noreply.github.com>
2024-04-23 22:43:58 +0200

    histograma bien hecho

--- CHEESEBURGAMES/Assets/Plugins/TelemetrySystem/Serializer/SerializerStrategies/CSVSerialize.cs
+++ CHEESEBURGAMES/Assets/Plugins/TelemetrySystem/Serializer/SerializerStrategies/CSVSerialize.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class CSVSerialize : ISerializer
 {
     List<string> eventVariables= new List<string>();
-
+    const char separator = ';';
     public string EndFileFormat()
     {
         return "";
@@ -20,7 +20,7 @@ public class CSVSerialize : ISerializer
         string finalString = "";
         for(int i=0; i<eventVariables.Count-1; i++)
         {
-            finalString += eventVariables[i] + ";";
+            finalString += eventVariables[i] + separator;
         }
         finalString += eventVariables[eventVariables.Count-1];
         eventVariables.Clear();
